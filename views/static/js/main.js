6630bf91513de2477c37b55d7a0b1e906c8d6536
kant1724 <sonseungbum@naver.com>
2018-12-10 12:18:14 +0900

    commit

---
+++ views/static/js/main.js
@@ -0,0 +1,23 @@
+function ajax(url, input_data, gubun, method) {
+	$.ajax(url, {
+		type: method,
+        data: input_data,
+        async: false,
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        dataType: 'json',
+        success: function (data, status, xhr) {
+        },
+        error: function (jqXhr, textStatus, errorMessage) {
+        	if(jqXhr.status==404) {
+        		alert(textStatus);
+            }
+        }
+    });
+}
+
+$(document).ready(function() {
+});
+
+function callApi() {
+	ajax('/', {"" : ""}, '', 'POST');
+}
