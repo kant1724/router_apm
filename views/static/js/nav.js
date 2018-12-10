fa929f1d8f0af8848eaf655bc11071ac9e0f1e30
kant1724 <sonseungbum83@gmail.com>
2018-12-10 14:57:39 +0900

    commit

--- views/static/js/nav.js
+++ views/static/js/nav.js
@@ -1,15 +1,15 @@
 function getNav1() {
-	var filePath = '/views/static/res/nav/nav1'
+	var filePath = '/views/static/res/html_common/nav/nav1.html'
 			xmlhttp = new XMLHttpRequest();
 			xmlhttp.open("GET", filePath, false);
 			xmlhttp.send(null);
 	var fileContent = xmlhttp.responseText;
-	
+
 	return fileContent;
 }
 
 function getNav2() {
-	var filePath = '/views/static/res/nav/nav2'
+	var filePath = '/views/static/res/html_common/nav/nav2.html'
 			xmlhttp = new XMLHttpRequest();
 			xmlhttp.open("GET", filePath, false);
 			xmlhttp.send(null);
