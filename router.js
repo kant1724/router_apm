6f632317b6cab0f112ffd46042ddb394e761efd9
kant1724 <sonseungbum83@gmail.com>
2018-12-10 15:38:00 +0900

    commit

--- router.js
+++ router.js
@@ -7,6 +7,10 @@ module.exports = function(app) {
 		res.render('views/templates/main.html');
 	});
 
+	app.get('/table', function(req, res) {
+		res.render('views/templates/table.html');
+	});
+
 	app.post('/select', urlencodedParser, function(req, res) {
 		var ret = require('/server/main').select(
 			function callback(board_num) {
---
+++ views/static/js/table.js
@@ -0,0 +1,26 @@
+function ajax(url, input_data, gubun, method) {
+	$.ajax(url, {
+		type: method,
+        data: input_data,
+        async: false,
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        dataType: 'json',
+        success: function (data, status, xhr) {
+        },
+        error: function (jqXhr, textStatus, errorMessage) {
+        	if(jqXhr.status==404) {
+        		alert(textStatus);
+            }
+        }
+    });
+}
+
+$(document).ready(function() {
+	$('#sidenav-main').append(getNav1());
+	$('#navbar-main').append(getNav2());
+	
+});
+
+function callApi() {
+	ajax('/', {"" : ""}, '', 'POST');
+}
