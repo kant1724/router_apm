31c5e447a447f31d6a5e44f21c4ae71f0e311575
kant1724 <sonseungbum83@gmail.com>
2018-12-10 14:05:43 +0900

    commit

---
+++ router.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+module.exports = function(app) {
+	app.get('/', function(req, res) {
+		res.render('views/templates/main.html');
+	});
+
+	app.post('/select', urlencodedParser, function(req, res) {
+		var ret = require('/server/main').select(
+			function callback(board_num) {
+				res.status(200).send({res: JSON.stringify(ret)});
+			}
+		);
+	});
+}
