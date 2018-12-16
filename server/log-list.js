b3c5ee0c1fddef2bd86efff6bb989fd7836e703a
kant1724 <sonseungbum83@gmail.com>
2018-12-16 19:52:16 +0900

    commit

--- server/log-list.js
+++ server/log-list.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 // 2. testDB 세팅
-mongoose.connect('mongodb://localhost:27017/testDB');
+mongoose.connect('mongodb://14.63.168.58:27017/router_log');
 // 3. 연결된 testDB 사용
 var db = mongoose.connection;
 // 4. 연결 실패
@@ -12,6 +12,7 @@ db.once('open', function() {
     console.log('Connected!');
 });
 
+/**
 // 6. Schema 생성. (혹시 스키마에 대한 개념이 없다면, 입력될 데이터의 타입이 정의된 DB 설계도 라고 생각하면 됩니다.)
 var student = mongoose.Schema({
     name : 'string',
@@ -33,7 +34,7 @@ newStudent.save(function(error, data){
         console.log('Saved!')
     }
 });
-
+**/
 // 10. Student 레퍼런스 전체 데이터 가져오기
 Student.find(function(error, students){
     console.log('--- Read all ---');
@@ -43,7 +44,7 @@ Student.find(function(error, students){
         console.log(students);
     }
 })
-
+/**
 // 11. 특정 아이디값 가져오기
 Student.findOne({_id:'585b777f7e2315063457e4ac'}, function(error,student){
     console.log('--- Read one ---');
@@ -77,10 +78,6 @@ Student.remove({_id:'585b7c4371110029b0f584a2'}, function(error,output){
     if(error){
         console.log(error);
     }
-
-    /* ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
-        어떤 과정을 반복적으로 수행 하여도 결과가 동일하다. 삭제한 데이터를 다시 삭제하더라도, 존재하지 않는 데이터를 제거요청 하더라도 오류가 아니기 때문에
-        이부분에 대한 처리는 필요없다. 그냥 삭제 된것으로 처리
-        */
     console.log('--- deleted ---');
 });
+**/
