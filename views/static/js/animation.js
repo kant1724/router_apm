8de73390517bd4b5186e16d25021fbf18150140c
kant1724 <sonseungbum83@gmail.com>
2018-12-23 11:34:14 +0900

    commit

--- views/static/js/animation.js
+++ views/static/js/animation.js
@@ -22,6 +22,10 @@ $(document).ready(function() {
 	$('#sidenav-main').append(getNav1());
 	$(".button-collapse").sideNav();
 
+	$('#router_1').click(function() {
+		$('#modalRelatedContent').modal();
+	});
+
 	startMonitoring();
 });
 
@@ -53,4 +57,15 @@ function selectLogListCallback(data) {
 	} else {
 		$('#router_1').css('background', 'red');
 	}
+	var obj = $('#log_list_row').children();
+	if (obj.length >= 5) {
+		$(obj[0]).remove();
+	}
+	var html = '<div class="col-7">';
+	 	  html += d[0].ip + ' ';
+			html += d[0].date + ' ';
+			html += d[0].hour + ':' + d[0].minute + ':' + d[0].seconds + ' ';
+			html += d[0].log;
+			html += '</div>';
+	$('#log_list_row').append(html);
 }
